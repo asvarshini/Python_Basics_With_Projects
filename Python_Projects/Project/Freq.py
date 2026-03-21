e9357827d61981bb70232f87c845f35cf467a699
VarshiniAS <asvarshini84@gmail.com>
2026-03-21 07:29:31 +0530

    Added count  frequency of words

---
+++ Python_Projects/Project/Freq.py
@@ -0,0 +1,14 @@
+sentence="Pyhton is a objectoriented langauage"
+words = sentence.split()
+freq = {}
+
+for word in words:
+    freq[word] = freq.get(word, 0) + 1
+
+print(freq)
+
+print(freq.keys()) # prints the keys of the dictionary
+print(freq.values()) # prints the values of the dictionary
+print(freq.items()) # prints the key-value pairs of the dictionary
+print(freq.get("python")) # prints the value of the key "python"
+print(freq.get("python", 0)) # prints the value of the key "python" if it exists, otherwise prints 0
